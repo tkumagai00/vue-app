b91a8bf701a324bbf3a2095012187025204a5798
tkumagai00 <tkumagai00@gmail.com>
2020-01-19 15:12:09 +0900

    フィルターの自作

---
+++ chap06/js/filter_br.js
@@ -0,0 +1,14 @@
+Vue.filter('nl2br', function(value) {
+    if (typeof value !== 'string') {
+        return value;
+    }
+    //return value.replace(/\r?\n/g, '<br />');
+    return value.replace(/\r?\n/g, '<br />');
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        memo: ''
+    }
+});
