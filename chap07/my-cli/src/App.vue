22ceb2ae3cd9533074ec49c40c27aa4aeda86b17
tkumagai00 <tkumagai00@gmail.com>
2020-01-26 11:45:07 +0900

    単一ファイルコンポーネント

--- chap07/my-cli/src/App.vue
+++ chap07/my-cli/src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <!--
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    -->
+    <p>こんにちは、Vue.js</p>
+
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+<style scoped>
+  p {
+    border: 1px solid Red;
+    background-color:Yellow;
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
+
+<!--<script>-->
+<!--import HelloWorld from './components/HelloWorld.vue'-->
+
+<!--export default {-->
+<!--  name: 'app',-->
+<!--  components: {-->
+<!--    HelloWorld-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<!--<style>-->
+<!--#app {-->
+<!--  font-family: 'Avenir', Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  text-align: center;-->
+<!--  color: #2c3e50;-->
+<!--  margin-top: 60px;-->
+<!--}-->
+<!--</style>-->
--- chap07/my-cli/src/main.js
+++ chap07/my-cli/src/main.js
@@ -5,4 +5,11 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 上は以下のコードの省略(匿名関数をアロー関数でシンプルに)
+  // render: function(h) {
+  //   return h(App);
+  // }
 }).$mount('#app')
+
+
+
