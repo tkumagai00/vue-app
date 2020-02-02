66696fa13fc5da05954d68351cd89c9f276992c8
tkumagai00 <tkumagai00@gmail.com>
2020-02-02 11:35:31 +0900

    Vuexの基本

---
+++ chap09/vuex-basic/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input type="button" value="-" v-on:click="minus" />
+    {{count}}
+    <input type="button" value="+" v-on:click="plus" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    minus() {
+      this.$store.commit('minus')
+    },
+    plus() {
+      this.$store.commit('plus')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
